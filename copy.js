3e152a39825fc2b75cd59c08c2f71b4b1e999b44
abhijay24 <abhijay24.3.7@gmail.com>
2020-07-06 21:01:22 +0530

    all about react

---
+++ copy.js
@@ -0,0 +1,38 @@
+/*<!DOCTYPE HTML>
+<html>
+ <head>
+  <title>my portfolio</title>
+  <!linking the style class-->
+  <link
+  rel = "stylesheet"
+  type = "text/css"
+  href = "style.css"/>
+ </head>
+ <body>
+  <div class="header">
+  <h2>Abhijay Srivastava</h2>
+   
+  <a href="aboutme.html">about me</a>
+  </div>
+  <div>
+  <a href="https://abhijay24.github.io/c40/index.html">projects</a>
+  <h5>games designed by me</h5>   
+  <p>these games are designed by me using matter.js library, p5.js library, p5.play.js library </p>
+  </div>
+  <div class = "game">
+  <p> raindrop </p>  
+  <a href="https://github.com/abhijay24/raindrops"><img src = "2020-06-09.png" width = 500 height = 400 />  raindrop game</a>
+  </div>
+  <div class = "game">
+    <p>
+   <a href="https://abhijay24.github.io/stick-castle1/"><img src = "2020-06-15.png" width = 500 height = 450 /> A stick castle</a>
+  </p>
+  </div>       
+  
+  
+  <a href = '#'><img src = "youtube.png" width = 70 height = 70><a>
+  <a href = '#'><img src = "insta.png" width = 70 height = 70><a> 
+  <a href = '#'><img src = "fb.png" width = 70 height = 70><a></a>*/
+
+
+  /*<a href = '#'><img src = "fb.png" width = 70 height = 70><a>
